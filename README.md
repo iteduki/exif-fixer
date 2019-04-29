9216a26522490fa02dc4feb91cd7575cb71f76e6
小川貴大 <mistolteen0127@gmail.com>
2019-04-29 22:31:16 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 使い方
+
+## ライブラリのインストール
+
+`brew install exiftool`
+
+`gem install mini_exiftool`
+
+## 実行
+
+ファイル名で順番を指定した画像を`images`に入れて実行
+
+`ruby code.rb`
---
+++ code.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'mini_exiftool'
+
+files = Dir.glob('images/*{jpg,jpeg,png}')
+p files.count
+
+current = DateTime.now
+files.each_with_index do |file, i|
+  p file
+  photo = MiniExiftool.new file
+  photo.date_time_original = current + Rational(i, 24 * 60) # 1分後
+  photo.save
+end
